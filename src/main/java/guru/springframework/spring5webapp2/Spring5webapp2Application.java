968ae8dfe83d24c554ac28ebd767f242f314aaa8
Kion <daniarkano@gmail.com>
2019-05-19 01:04:02 +0200

    Git initial exmaple JB2

---
+++ src/main/java/guru/springframework/spring5webapp2/Spring5webapp2Application.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapp2Application.class, args);
+	}
+}
